cd34739f9524788e9b623a4bd639caf71f456e9b
gcancho <giovannicb19@gmail.com>
2022-01-21 16:37:52 -0500

    update: ejercicio terminado

--- script.js
+++ script.js
@@ -2,7 +2,7 @@
 $("#barra").click(function () {
   $("#menu-toogle").toggleClass("active");
 });
-// Funciones para ocultar el menu al hacer click afuera
+// Funciones para ocultar el menu al hacer click afuera del menu en mobile
 $(window).click(function () {
   $("#menu-toogle").removeClass("active");
 });
@@ -14,4 +14,13 @@ $("#barra").click(function (event) {
 $("#buscar").click(function () {
   $("#input-buscador").addClass("active");
   $("#input-buscador").focus();
+});
+
+// Funciones para que aparezcan los modales
+$("#icon-carrito").click(function () {
+  $("#modal1").modal();
+});
+
+$("#icon-cuenta").click(function () {
+  $("#modal2").modal();
 });
